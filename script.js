59df224f3a2e46126988bf725b03076296ebd627
Nik Kolesnikov <niknills94@gmail.com>
2022-08-18 17:37:21 +0300

    feat: added task 2 solution

--- script.js
+++ script.js
@@ -22,3 +22,36 @@ if (enterSecondNumber - enterFirstNumber >= 4 || enterFirstNumber - enterSecondN
     }
   }
 }
+
+const pinCode = "1234";
+const pucCode = "1234567890";
+
+let enterPinCode, enterPucCode;
+
+pin: for (let i = 1; i <= 3; i++) {
+  enterPinCode = prompt("Please enter PIN code", "");
+
+  if (enterPinCode !== pinCode) {
+    console.log(
+      `PIN code entered incorrectly. You have ${3 - i} attempts left`
+    );
+  } else {
+    console.log("PIN code entered correctly");
+    break pin;
+  }
+}
+
+puc: if (enterPinCode !== pinCode) {
+  for (let i = 1; i <= 3; i++) {
+    enterPucCode = prompt("Please enter PUC code", "");
+
+    if (enterPucCode !== pucCode) {
+      console.log(
+        `PUC code entered incorrectly. You have ${3 - i} attempts left`
+      );
+    } else {
+      console.log("PUC code entered correctly");
+      break puc;
+    }
+  }
+}
